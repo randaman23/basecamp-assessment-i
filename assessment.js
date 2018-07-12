0dfb9bd09d52a74bd2972626c0544971dbd3fede
randaman23 <randaman234@gmail.com>
2018-07-12 17:47:09 -0600

    test

--- assessment.js
+++ assessment.js
@@ -1,6 +1,7 @@
 // #1 create a variable called 'myName' and assign it to your name as a string
 
 // CODE HERE
+var myName= 'Randall';
 
 // #2 create a variable called 'faveNum' and assign it to your favorite number
 
